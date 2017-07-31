dc5bb604d39beb62b98e46f7f19f246118830c14
Adam Jacquier-Parr <aljparr0@gmail.com>
2017-07-31 17:37:15 +1000

    Update interface for api, change how numbers and safe numbers are generated to conform with all tests.

--- faker_e164/providers.py
+++ faker_e164/providers.py
@@ -1,16 +1,17 @@
 """
 Provider for faker to generate E164 compatible phone numbers.
 
-`safe_numbers` provided by https://fakenumber.org/australia/mobile
+"safe_numbers" provided by https://fakenumber.org/, the GB/United Kingdom "safe_numbers" are reported as invalid by the phonenumbers package.
 """
+import os
 import logging
 import phonenumbers
 from phonenumbers import PhoneNumber
 
 from faker.providers import BaseProvider
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
-
-
 
 safe_numbers = {
     'AU': [
@@ -29,37 +30,88 @@ safe_numbers = {
         '+12025550124',
     ],
     'GB': [
-		'+441632960600',
-		'+441632960541',
-		'+441632960702',
-		'+441632960979',
-		'+441632960570',
-		'+441632960864',
+        '+441632960600',
+        '+441632960541',
+        '+441632960702',
+        '+441632960979',
+        '+441632960570',
+        '+441632960864',
     ],
     'CA': [
-		'+16135550110',
-		'+16135550120',
-		'+16135550109',
-		'+16135550151',
-		'+16135550136',
-		'+16135550119',
+        '+16135550110',
+        '+16135550120',
+        '+16135550109',
+        '+16135550151',
+        '+16135550136',
+        '+16135550119',
     ]
 }
 
 
 class E164Provider(BaseProvider):
     """
-    Provider to generate random yet possible phone numbers for various countries
+    Provider to generate random phone numbers for various countries
     
     >>> from faker import Faker
     >>> from faker_e164.providers import E164Provider
     >>> fake = Faker()
     >>> fake.add_provider(E164Provider)
+    >>> phone_number = fake.e164()
     """
 
-    _e164_numerify_pattern = '%###########!!!'
+    _e164_numerify_pattern = '%######!!!!!!!!'  # https://en.wikipedia.org/wiki/E.164
 
-    def _e164_phone_number(self, region_code, is_valid=True, is_possible=True):
+    def _get_e164_numerify_pattern(self, region_code: str, is_possible=True):
+        if not is_possible:
+            return '#!!!!!!'
+        country_code = phonenumbers.country_code_for_region(region_code)
+        return str(country_code)+self._e164_numerify_pattern[len(str(country_code)):]
+
+    def _e164(self, region_code: str, is_valid=True, is_possible=True) -> PhoneNumber:
+        """
+        Generate an e164 phone number
+        """
+        assert not (is_valid and not is_possible), 'is_valid must be False if is_possible is False'
+        e164_numerify_pattern = self._get_e164_numerify_pattern(region_code, is_possible=is_possible)
+        phone_number = self.numerify(e164_numerify_pattern)
+        while not isinstance(phone_number, PhoneNumber):
+            try:
+                phone_number = phonenumbers.parse(phone_number, region_code)
+            except phonenumbers.phonenumberutil.NumberParseException:
+                phone_number = self.numerify(e164_numerify_pattern)
+                continue
+
+            if is_valid and not phonenumbers.is_valid_number(phone_number):
+                phone_number = self.numerify(e164_numerify_pattern)
+                continue
+            elif not is_valid and phonenumbers.is_valid_number(phone_number):
+                phone_number = self.numerify(e164_numerify_pattern)
+                continue
+
+            if is_possible and not phonenumbers.is_possible_number(phone_number):
+                phone_number = self.numerify(e164_numerify_pattern)
+                continue
+            elif not is_possible and phonenumbers.is_possible_number(phone_number):
+                phone_number = self.numerify(e164_numerify_pattern)
+                continue
+
+        return phone_number
+
+    def e164(self, region_code: str = None, valid=True, possible=True) -> str:
+        """Return a random e164 formatted phone number"""
+        if region_code is None:
+            region_code = self.random_element(phonenumbers.SUPPORTED_REGIONS)
+        return phonenumbers.format_number(self._e164(region_code, is_valid=valid, is_possible=possible), phonenumbers.PhoneNumberFormat.E164)
+
+    def safe_e164(self, region_code: str = None) -> str:
+        """Return a "safe" e164 phone number"""
+        if region_code is None:
+            region_code = self.random_element(list(safe_numbers.keys()))
+        phone_number = phonenumbers.parse(self.random_element(safe_numbers[region_code]))
+        return phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164)
+
+    # Deprecated methods
+    def _e164_phone_number(self, region_code, is_valid=True, is_possible=True):  # pragma: no cover
         """
         Generate an e164 phone number
 
@@ -67,29 +119,7 @@ class E164Provider(BaseProvider):
         """
         return self._e164(region_code, is_valid=is_valid, is_possible=is_possible)
 
-    def _e164(self, region_code: str, is_valid=True, is_possible=True) -> PhoneNumber:
-        """
-        Generate an e164 phone number
-        """
-        phone_number = self.numerify(self._e164_numerify_pattern)
-        while not isinstance(phone_number, PhoneNumber):
-            try:
-                phone_number = phonenumbers.parse(phone_number, region_code)
-            except phonenumbers.phonenumberutil.NumberParseException:
-                phone_number = self.numerify(self._e164_numerify_pattern)
-                continue
-
-            if is_valid and not phonenumbers.is_valid_number(phone_number):
-                phone_number = self.numerify(self._e164_numerify_pattern)
-                continue
-
-            if is_possible and not phonenumbers.is_possible_number(phone_number):
-                phone_number = self.numerify(self._e164_numerify_pattern)
-                continue
-
-        return phone_number
-
-    def invalid_e164_phone_number(self, country=None):
+    def invalid_e164_phone_number(self, country=None):  # pragma: no cover
         """
         Return an invalid e164 phone number
 
@@ -98,12 +128,10 @@ class E164Provider(BaseProvider):
         logger.warning('invalid_e164_phone_number(...) will be deprecated in favor of the .e164(...) method.')
         countries = ['AU', 'US', 'GB', 'NZ']
         country = country or self.random_element(countries)
-        phone_number = self._e164_phone_number(country, is_valid=False,
-                                               is_possible=True)
-        return phonenumbers.format_number(phone_number,
-                                          phonenumbers.PhoneNumberFormat.E164)
+        phone_number = self._e164_phone_number(country, is_valid=False, )
+        return phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164)
 
-    def valid_e164_phone_number(self, country=None):
+    def valid_e164_phone_number(self, country=None):  # pragma: no cover
         """
         Return an invalid e164 phone number.
 
@@ -112,17 +140,5 @@ class E164Provider(BaseProvider):
         logger.warning('valid_e164_phone_number(...) will be deprecated in favor of the .e164(...) method.')
         countries = ['AU', 'US', 'GB', 'NZ']
         country = country or self.random_element(countries)
-        phone_number = self._e164_phone_number(country, is_valid=True,
-                                               is_possible=True)
-        return phonenumbers.format_number(phone_number,
-                                          phonenumbers.PhoneNumberFormat.E164)
-
-    def e164(self, region_code: str=None, valid=True, possible=True):
-        """Return a random e164 formatted phone number"""
-        if region_code is None:
-            region_code = self.random_element(phonenumbers.SUPPORTED_REGIONS)
-        return self._e164_phone_number(region_code, is_valid=valid, is_possible=possible)
-
-    def safe_e164(self, region_code: str=None):
-        """Return a "safe" e164 phone number"""
-        return self.random_element(safe_numbers[region_code])
+        phone_number = self._e164_phone_number(country, is_valid=True, )
+        return phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164)
--- tests/test_providers.py
+++ tests/test_providers.py
@@ -1,24 +1,96 @@
 """
 Test the E164Provider class
 """
-from unittest import mock, TestCase
+import os
+import logging
 
+from typing import Union
+from unittest import TestCase, mock
+
+import phonenumbers
+
+from faker import Faker
+
+from faker_e164 import providers
 from faker_e164.providers import E164Provider
+
+logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO'))
+logger = logging.getLogger(__name__)
 
 
 class E164PhoneNumberProviderTestCase(TestCase):
 
-    def test_valid_e164_phone_number(self):
-        provider = E164Provider(None)
-        phone_number = provider.valid_e164_phone_number()
+    provider_class = E164Provider
 
-        with mock.patch('faker.providers.BaseProvider.numerify') as numerify:
-            def g():
-                yield 'badnumber'
-                yield '+61426814275'
-            numerify.side_effect = iter(g())
-            provider.valid_e164_phone_number()
+    def test_e164(self):
+        """Test generating e164 phone numbers for all regions"""
+        fake = Faker()  # type: Union[E164Provider, Faker]
+        fake.add_provider(self.provider_class)
+        for region_code in phonenumbers.SUPPORTED_REGIONS:
+            phone_number = fake.e164(region_code=region_code)
+            self.assertIsInstance(phone_number, str)
+            phone_number = phonenumbers.parse(phone_number)
+            self.assertTrue(phonenumbers.is_valid_number(phone_number))
+        phone_number = fake.e164()
+        self.assertIsInstance(phone_number, str)
+        phone_number = phonenumbers.parse(phone_number)
+        self.assertTrue(phonenumbers.is_valid_number(phone_number))
 
-    def test_invalid_e164_phone_number(self):
-        provider = E164Provider(None)
-        phone_number = provider.invalid_e164_phone_number()
+    def test_e164_is_valid_false(self):
+        """Test generating e164 phone numbers for all regions where valid=False"""
+        fake = Faker()  # type: Union[E164Provider, Faker]
+        fake.add_provider(self.provider_class)
+        for region_code in phonenumbers.SUPPORTED_REGIONS:
+            phone_number = fake.e164(region_code=region_code, valid=False, possible=True, )
+            self.assertIsInstance(phone_number, str)
+            phone_number = phonenumbers.parse(phone_number)
+            self.assertFalse(phonenumbers.is_valid_number(phone_number))
+        phone_number = fake.e164(valid=False, possible=True, )
+        self.assertIsInstance(phone_number, str)
+        phone_number = phonenumbers.parse(phone_number)
+        self.assertFalse(phonenumbers.is_valid_number(phone_number))
+
+    def test_e164_is_possible_false(self):
+        """Test generating e164 phone numbers for all regions where valid=False and possible=False"""
+        fake = Faker()  # type: Union[E164Provider, Faker]
+        fake.add_provider(self.provider_class)
+        for region_code in phonenumbers.SUPPORTED_REGIONS:
+            phone_number = fake.e164(region_code=region_code, valid=False, possible=False, )
+            self.assertIsInstance(phone_number, str)
+            phone_number = phonenumbers.parse(phone_number)
+            self.assertFalse(phonenumbers.is_possible_number(phone_number))
+        phone_number = fake.e164(valid=False, possible=False, )
+        self.assertIsInstance(phone_number, str)
+        phone_number = phonenumbers.parse(phone_number)
+        self.assertFalse(phonenumbers.is_possible_number(phone_number))
+
+    def test_e164_asserts_is_possible_true_if_is_valid_false(self):
+        """Regression test that an assertion error is raised for bad `valid` and `possible` arguments"""
+        fake = Faker()  # type: Union[E164Provider, Faker]
+        fake.add_provider(self.provider_class)
+        with self.assertRaises(AssertionError):
+            fake.e164(valid=True, possible=False)
+
+    def test_safe_e164(self):
+        """Test returning all `safe_numbers`
+
+        Set the `LOGLEVEL` environment variable to 'DEBUG' to see the phone number info for `safe_numbers`"""
+        fake = Faker()  # type: Union[E164Provider, Faker]
+        fake.add_provider(self.provider_class)
+
+        phone_number = fake.safe_e164()
+        self.assertIsInstance(phone_number, str)
+
+        for region_code in providers.safe_numbers.keys():
+            with mock.patch('faker_e164.providers.E164Provider.random_element') as random_element:
+                random_element.side_effect = providers.safe_numbers[region_code]
+                for _ in range(len(providers.safe_numbers[region_code])):
+                    phone_number = fake.safe_e164(region_code=region_code)
+                    self.assertIsInstance(phone_number, str)
+                    if os.environ.get('LOGLEVEL', None) == 'DEBUG':
+                        phone_number = phonenumbers.parse(phone_number)
+                        logger.debug('Phone Number info: {}\nis_valid: {is_valid}\nis_possible: {is_possible}'.format(
+                            phone_number,
+                            is_valid=phonenumbers.is_valid_number(phone_number),
+                            is_possible=phonenumbers.is_possible_number(phone_number)
+                        ))
